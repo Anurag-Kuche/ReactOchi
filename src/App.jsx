a909279b0844a5906c80f5aa335c6585c93c28cd
Anurag-kuche <anuragkuche1@gmail.com>
2025-01-10 23:42:43 +0530

    Added About component and modified App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@
 import LandingPage from './components/LandingPage'
 import Navbar from './components/Navbar'
 import Marquee from './components/Marquee'
+import About from './components/About'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
       <Navbar />
       <LandingPage />
       <Marquee />
+      <About />
     </div>
   )
 }
---
+++ src/components/About.jsx
@@ -0,0 +1,12 @@
+
+
+function About() {
+  return (
+    <div className="w-full p-20 bg-[#CDEA68] text-black">
+        <h1 className="font-['Neue_Montreal'] text-6xl" >Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell products, explain complex ideas, and hire great people</h1>
+        <div className="w-full border-t-[1px] mt-20 border-[#738146] "></div>
+    </div>
+  )
+}
+
+export default About
