f5bce05cea609cb0d94c3a7a232e286eed870a7c
Anurag-kuche <anuragkuche1@gmail.com>
2025-01-11 23:42:20 +0530

    Added Eyes component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -4,7 +4,15 @@ function About() {
   return (
     <div className="w-full p-20 bg-[#CDEA68] text-black">
         <h1 className="font-['Neue_Montreal'] text-6xl" >Ochi is a strategic presentation agency for forward-thinking businesses that need to raise funds, sell products, explain complex ideas, and hire great people</h1>
-        <div className="w-full border-t-[1px] mt-20 border-[#738146] "></div>
+        <div className="w-full flex gap-5 border-t-[1px] pt-10 mt-20 border-[#738146] ">
+          <div className="w-1/2">
+            <h1 className="text-7xl">Our approach :</h1>
+            <button className="flex gap-10 uppercase items-center px-10 py-6 bg-zinc-900 mt-10 rounded-full text-white">Read More
+              <div className="w-2 h-2 bg-zinc-100 rounded-full"></div>
+            </button>
+          </div>
+          <div className="w-1/2 h-[70vh] rounded-3xl bg-[url('')]]"></div>  
+        </div>
     </div>
   )
 }
