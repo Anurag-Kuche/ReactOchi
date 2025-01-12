ab81cb49a73010b0d7e93ba3713a98afa888dd93
Anurag-kuche <anuragkuche1@gmail.com>
2025-01-12 23:26:44 +0530

    New changes

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar'
 import Marquee from './components/Marquee'
 import About from './components/About'
 import Eyes from './components/Eyes'
+import Featured from './components/Featured'
 function App() {
   return (
     <div className='w-full h-screen bg-zinc-900 text-white'>
@@ -12,6 +13,7 @@ function App() {
       <Marquee />
       <About />
       <Eyes />
+      <Featured />
     </div>
   )
 }
---
+++ src/components/Featured.jsx
@@ -0,0 +1,12 @@
+
+function Featured() {
+  return (
+    <div className="w-full py-10">
+        <div className="w-full px-10">
+            <h1 className="text-8xl ">Featured Projects</h1>
+        </div>
+    </div>
+  )
+}
+
+export default Featured
