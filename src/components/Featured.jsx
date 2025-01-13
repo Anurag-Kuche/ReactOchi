5d2529e267a9dcc4fec2096c65cc4d2192fe19e1
Anurag-kuche <anuragkuche1@gmail.com>
2025-01-13 23:43:20 +0530

    New Changes in Featured section

--- src/components/Featured.jsx
+++ src/components/Featured.jsx
@@ -1,12 +1,31 @@
-
 function Featured() {
   return (
-    <div className="w-full py-10">
-        <div className="w-full px-10">
-            <h1 className="text-8xl ">Featured Projects</h1>
+    <div className='w-full py-10'>
+      <div className='w-full px-20 border-b border-zinc-700 pb-20'>
+        <h1 className='text-7xl tracking-tight mb-10'>Featured Projects</h1>
+        
+        <div className="cards w-full flex gap-10">
+          {/* Card 1 */}
+          <div className="cardcontainer w-1/2 h-[80vh] rounded-lg overflow-hidden bg-red-700">
+            <img 
+              className='w-full h-full object-cover' 
+              src="https://ochi.design/wp-content/uploads/2024/08/CS_Website_1-663x551.png" 
+              alt="Featured Project 1" 
+            />
+          </div>
+
+          {/* Card 2 */}
+          <div className="cardcontainer w-1/2 h-[80vh] rounded-lg overflow-hidden bg-blue-700">
+            <img 
+              className='w-full h-full object-cover' 
+              src="https://ochi.design/wp-content/uploads/2022/09/Vise_front2-663x551.jpg" 
+              alt="Featured Project 2" 
+            />
+          </div>
         </div>
+      </div>
     </div>
   )
 }
 
-export default Featured
+export default Featured;
